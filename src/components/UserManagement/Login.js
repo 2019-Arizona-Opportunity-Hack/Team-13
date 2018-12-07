12f29f566b9ba6d311edb869e5f27c39899b98cd
dapperauteur <a@awews.com>
2018-12-07 17:59:03 -0500

    add security reducer and add Login logic

--- src/components/UserManagement/Login.js
+++ src/components/UserManagement/Login.js
@@ -17,15 +17,15 @@ class Login extends Component {
   }
 
   componentDidMount() {
-    // if (this.props.security.validToken) {
-    //   this.props.history.push("/dashboard");
-    // }
+    if (this.props.security.validToken) {
+      this.props.history.push("/dashboard");
+    }
   }
 
   componentWillReceiveProps(nextProps) {
-    // if (nextProps.security.validToken) {
-    //   this.props.history.push("/dashboard");
-    // }
+    if (nextProps.security.validToken) {
+      this.props.history.push("/dashboard");
+    }
     if (nextProps.errors) {
       console.log(nextProps.errors);
       this.setState({ errors: nextProps.errors });
