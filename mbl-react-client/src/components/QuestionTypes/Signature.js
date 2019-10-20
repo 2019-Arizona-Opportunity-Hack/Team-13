2ef366a1a564fe8dca57543a4d38b2d27c43bab7
Angel sopena <angelsopena@Angels-MacBook-Pro.local>
2019-10-20 11:55:43 -0700

    create signatures

--- mbl-react-client/src/components/QuestionTypes/Signature.js
+++ mbl-react-client/src/components/QuestionTypes/Signature.js
@@ -2,10 +2,32 @@ import React, { Component } from "react";
 import CanvasDraw from "react-canvas-draw";
 
 export default class Signature extends Component {
+  constructor(props) {
+    super(props);
+    this.save = this.save.bind(this);
+    this.clear = this.clear.bind(this);
+  }
+  save() {
+    if (!this.saveableCanvas) {
+      return; // can't save yet
+    }
+    // ASSUME they signed and it isn't blank
+    const signature = this.saveableCanvas.getSaveData();
+    debugger;
+    this.props.onChange(signature);
+  }
+  clear() {
+    this.saveableCanvas.clear();
+  }
   render() {
     return (
       <div>
-        <CanvasDraw loadTimeOffset={10} />
+        <CanvasDraw
+          ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
+          loadTimeOffset={10}
+        />
+        <button onClick={this.save}>Save</button>
+        <button onClick={this.clear}>Clear</button>
       </div>
     );
   }
